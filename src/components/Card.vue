cb0b87789f3d8c3f35a8b733c82ae7c72a2008b9
Iván Guerrero <ivanguebri@gmail.com>
2024-08-21 14:31:06 -0500

    feat(card): custom card

---
+++ src/components/Card.vue
@@ -0,0 +1,27 @@
+<script setup>
+import Card from "primevue/card";
+
+const preset = {
+  root: {
+    class: ["rounded-[10px]", "bg-zinc-900", "text-violet-400"],
+  },
+  body: {
+    class: ["!p-5", "!gap-0"],
+  },
+};
+
+const ptOptions = {
+  mergeSections: true,
+  mergeProps: true,
+};
+</script>
+<template>
+  <Card :pt="preset" :pt-options="ptOptions">
+    <template #title>
+      <slot name="title"></slot>
+    </template>
+    <template #content>
+      <slot name="content"></slot>
+    </template>
+  </Card>
+</template>

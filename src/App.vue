aed4083f8163b2a3cb53c0c54b23b4cbfb1f8088
Iván Guerrero <ivanguebri@gmail.com>
2024-08-22 17:02:20 -0500

    feat: finish the details of the app

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,80 @@
 <script setup>
 import { computed, ref } from "vue";
-import Item from "./components/app/Item.vue";
 
-const items = ref([
-  { id: 1, text: "Learn Vue 3", done: false },
-  { id: 2, text: "Learn Pinia", done: true },
-  { id: 3, text: "Learn Vite", done: false },
-]);
+import Button from "@/components/primevue/Button.vue";
+import Input from "@/components/primevue/Input.vue";
+
+import Item from "@/components/app/Item.vue";
+
+const items = ref([]);
+const todoInputModel = ref();
 
 const tasksToDo = computed(() => items.value.filter((item) => !item.done));
 const tasksDone = computed(() => items.value.filter((item) => item.done));
-// margins collapse so there's no problem when both lists are present in the DOM
-const tasksTodoClass = computed(() => ({
-  "flex flex-col gap-4": true,
-  "mb-14": tasksDone.value.length > 0,
-}));
-const tasksDoneClass = computed(() => ({
-  "flex flex-col gap-4": true,
-  "mt-14": tasksToDo.value.length > 0,
-}));
+
+function addTodo(text) {
+  items.value.push({ id: Date.now(), text, done: false });
+}
+function markTodo(id) {
+  const toMarkTodoIdx = items.value.findIndex((item) => item.id === id);
+  if (toMarkTodoIdx > -1)
+    items.value.splice(toMarkTodoIdx, 1, {
+      ...items.value[toMarkTodoIdx],
+      done: true,
+    });
+}
+function deleteTodo(id) {
+  const toDeleteTodoIdx = items.value.findIndex((item) => item.id === id);
+  if (toDeleteTodoIdx > -1) items.value.splice(toDeleteTodoIdx, 1);
+}
+
+function onSubmit() {
+  if (todoInputModel.value.trim() === "") return;
+  addTodo(todoInputModel.value);
+  todoInputModel.value = undefined;
+}
 </script>
 
 <template>
-  <div class="min-h-screen bg-zinc-950">
-    <template v-if="tasksToDo.length > 0">
-      <div :class="tasksTodoClass">
-        <h2 class="text-white text-base">
-          Tasks to do - {{ tasksToDo.length }}
-        </h2>
-        <Item
-          v-for="todoItem in tasksToDo"
-          :key="todoItem.id"
-          :text="todoItem.text"
-          :done="todoItem.done"
-        />
-      </div>
-    </template>
-    <template v-if="tasksDone.length > 0">
-      <div :class="tasksDoneClass">
-        <h2 class="text-white text-base">Done - {{ tasksDone.length }}</h2>
-        <Item
-          v-for="doneItem in tasksDone"
-          :key="doneItem.id"
-          :text="doneItem.text"
-          :done="doneItem.done"
-        />
-      </div>
-    </template>
+  <div class="min-h-screen bg-zinc-950 pt-16 px-4">
+    <div class="flex flex-col gap-14 mx-auto max-w-[800px]">
+      <form @submit.prevent="onSubmit()">
+        <div class="flex gap-3">
+          <Input
+            fluid
+            placeholder="Add a new task"
+            required
+            v-model="todoInputModel"
+          />
+          <Button type="submit" icon="pi pi-plus" />
+        </div>
+      </form>
+      <template v-if="tasksToDo.length > 0">
+        <div class="flex flex-col gap-4">
+          <h2 class="text-white text-base">
+            Tasks to do - {{ tasksToDo.length }}
+          </h2>
+          <Item
+            v-for="todoItem in tasksToDo"
+            :key="todoItem.id"
+            :todo="todoItem"
+            :markTodoFn="markTodo"
+            :deleteTodoFn="deleteTodo"
+          />
+        </div>
+      </template>
+      <template v-if="tasksDone.length > 0">
+        <div class="flex flex-col gap-4">
+          <h2 class="text-white text-base">Done - {{ tasksDone.length }}</h2>
+          <Item
+            v-for="doneItem in tasksDone"
+            :key="doneItem.id"
+            :todo="doneItem"
+            :markTodoFn="markTodo"
+            :deleteTodoFn="deleteTodo"
+          />
+        </div>
+      </template>
+    </div>
   </div>
 </template>

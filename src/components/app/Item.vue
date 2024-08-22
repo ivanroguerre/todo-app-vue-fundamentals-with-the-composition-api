e0b2060711a95b2512f6594be6fd8898cf321b37
Iván Guerrero <ivanguebri@gmail.com>
2024-08-22 10:55:36 -0500

    refactor(Item): general Item component
    Instead of using ToDoItem and DoneItem use a general Item component that
    can be used for both.

--- src/components/app/Item.vue
+++ src/components/app/Item.vue
@@ -2,18 +2,28 @@
 import Button from "../primevue/Button.vue";
 import Card from "../primevue/Card.vue";
 
-const { text } = defineProps({
+const { text, done } = defineProps({
   text: String,
+  done: Boolean,
 });
+
+const textClass = {
+  "line-through": done,
+  "text-emerald-300": done,
+};
+const buttonsClass = {
+  flex: true,
+  hidden: done,
+};
 </script>
 <template>
   <Card>
     <template #title>
       <div class="flex justify-between items-start gap-4">
-        <div>
+        <div :class="textClass">
           {{ text }}
         </div>
-        <div class="flex">
+        <div :class="buttonsClass">
           <Button icon="pi pi-check" text />
           <Button icon="pi pi-trash" text />
         </div>
